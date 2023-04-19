7d0e6e32687334eefdd84b2a9bbea8b657ac3607
hagan23 <haganemmanuel23@gmail.com>
2023-04-19 12:06:36 +0000

    Add isItUppercase

--- src/validation.js
+++ src/validation.js
@@ -2,6 +2,7 @@ export const email = document.querySelector('.contact__email');
 export const form = document.querySelector('#form');
 export const error = document.querySelector('.error');
 
+//isItUppercase
 export function isItUppercase(value) {
   if (value.match(/^[a-z@.0-9-_]*$/)) {
     return true;
